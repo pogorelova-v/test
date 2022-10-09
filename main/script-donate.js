ef3340ace52a99f2464ebb33c639ba557b2496e2
Pogorelova-v <gorelikovavn@gmail.com>
2022-10-09 13:46:27 +0300

    df

--- main/script-donate.js
+++ main/script-donate.js
@@ -27,6 +27,23 @@ footerA.forEach(el =>
     })
 )
 
+//-------------------popup
+
+const burgerButton = document.querySelector('.burger-button');
+const wrapperPopapBurger = document.querySelector('.wrapper-popap-burger');
+const wrapperBurgerClose = document.querySelector('.wrapper-burger-close');
+const ixsPopapClose = document.querySelector('.ixs-popap-close');
+
+burgerButton.addEventListener('click', function(){
+  wrapperPopapBurger.classList.add('wrapper-popap-burger-open')
+});
+wrapperBurgerClose.addEventListener('click', function(){
+  wrapperPopapBurger.classList.remove('wrapper-popap-burger-open')
+});
+ixsPopapClose.addEventListener('click', function(){
+  wrapperPopapBurger.classList.remove('wrapper-popap-burger-open')
+});
+
 // --------- validation form
 
 const fni = document.getElementById("fni");
@@ -70,7 +87,8 @@ stepBarElipse.forEach(el =>
                 ind = i;
             }
         } 
-        stepBarElipse[ind].classList.remove('step-bar-elipse-active');        stepBarSum[ind].classList.remove('step-bar-sum-active');
+        stepBarElipse[ind].classList.remove('step-bar-elipse-active');       
+         stepBarSum[ind].classList.remove('step-bar-sum-active');
     })
 )
 
@@ -89,12 +107,76 @@ stepBarElipse.forEach(el =>
 )
 
 
-//----------------------------------
+//anotherAmountInpute.value = 
+
+//---------------------------------- anotherAmountInpute 
 
 
-const anotherAmountInpute = document.getElementById('anotherAmountInpute')
+let anotherAmountInpute = document.getElementById('anotherAmountInpute')
+
+stepBarElipse[0].addEventListener('click', () => {
+    anotherAmountInpute.value = 5000;
+})
+stepBarElipse[1].addEventListener('click', () => {
+    anotherAmountInpute.value = 2000;
+})
+stepBarElipse[2].addEventListener('click', () => {
+    anotherAmountInpute.value = 1000;
+})
+stepBarElipse[3].addEventListener('click', () => {
+    anotherAmountInpute.value = 500;
+})
+stepBarElipse[4].addEventListener('click', () => {
+    anotherAmountInpute.value = 250;
+})
+stepBarElipse[5].addEventListener('click', () => {
+    anotherAmountInpute.value = 100;
+})
+stepBarElipse[6].addEventListener('click', () => {
+    anotherAmountInpute.value = 50;
+})
+stepBarElipse[7].addEventListener('click', () => {
+    anotherAmountInpute.value = 25;
+})
+
+
 anotherAmountInpute.oninput = function () {
     if (this.value.length > 4) {
         this.value = this.value.slice(0,4); 
-    }
+    }    
+}
+anotherAmountInpute.oninput = function () {
+
+    stepBarElipse.forEach(el =>{
+        el.classList.remove('step-bar-elipse-active-click');
+    })
+    stepBarSum.forEach(el =>{
+        el.classList.remove('step-bar-sum-active-click');
+    })
+
+    if(this.value == 5000){
+      stepBarElipse[0].classList.add('step-bar-elipse-active-click');
+      stepBarSum[0].classList.add('step-bar-sum-active-click');
+    } else if(this.value == 2000){
+        stepBarElipse[1].classList.add('step-bar-elipse-active-click');
+        stepBarSum[1].classList.add('step-bar-sum-active-click');
+    } else if(this.value == 1000){
+        stepBarElipse[2].classList.add('step-bar-elipse-active-click');
+        stepBarSum[2].classList.add('step-bar-sum-active-click');
+    } else if(this.value == 500){
+        stepBarElipse[3].classList.add('step-bar-elipse-active-click');
+        stepBarSum[3].classList.add('step-bar-sum-active-click');
+    } else if(this.value == 250){
+        stepBarElipse[4].classList.add('step-bar-elipse-active-click');
+         stepBarSum[4].classList.add('step-bar-sum-active-click');
+    } else if(this.value == 100){
+        stepBarElipse[5].classList.add('step-bar-elipse-active-click');
+        stepBarSum[5].classList.add('step-bar-sum-active-click');
+    } else if(this.value == 50){
+         stepBarElipse[6].classList.add('step-bar-elipse-active-click');
+         stepBarSum[6].classList.add('step-bar-sum-active-click');
+    } else if(this.value == 25){
+        stepBarElipse[7].classList.add('step-bar-elipse-active-click');
+        stepBarSum[7].classList.add('step-bar-sum-active-click');
+    } 
 }
--- main/script.js
+++ main/script.js
@@ -30,43 +30,6 @@ petsCard.forEach(el =>
     })
 )
 
-//-------------scroll progres
-
-let scrollElem = testimonialslineScroll.querySelector('.testimonialsline-scroll-elem')
-
-scrollElem.onmousedown = function(event) { // слушаем нажатие на бегунок
-    event.preventDefault(); // убираем действие браузера по умолчанию
-
-  let shiftX = event.clientX - scrollElem.getBoundingClientRect().left; // переменная на изменение координат в окне
-
-    document.addEventListener('mousemove', onMouseMove); // при движении идем в функцию
-    document.addEventListener('mouseup', onMouseUp); // при остановке идём в функцию
-
-  function onMouseMove(event) {  // функция движения
-        let newLeft = event.clientX - shiftX - testimonialslineScroll.getBoundingClientRect().left;  // новая координата от движения
-
-        // заставляем бегунок остаться в границах слайдера.
-        if (newLeft < 0) {
-          newLeft = 0;
-        }
-        let rightEdge = testimonialslineScroll.offsetWidth - scrollElem.offsetWidth;
-        if (newLeft > rightEdge) {
-          newLeft = rightEdge;
-        }
-
-        scrollElem.style.left = newLeft + 'px';
-      }
-
-      function onMouseUp() {
-        document.removeEventListener('mouseup', onMouseUp);
-        document.removeEventListener('mousemove', onMouseMove);
-      }
-
-    };
-
-scrollElem.ondragstart = function() { // отключаем Drag’n’Drop браузера
-      return false;
-};
 
 //   изменение цвета картинки соцсетей по наведению
 
@@ -111,3 +74,145 @@ fni.addEventListener("input", function (event) {
     fnb.classList.remove('footer-news-button-error')
   }
 });
+
+
+//-----------------------popup
+
+const burgerButton = document.querySelector('.burger-button');
+const wrapperPopapBurger = document.querySelector('.wrapper-popap-burger');
+const wrapperBurgerClose = document.querySelector('.wrapper-burger-close');
+const ixsPopapClose = document.querySelector('.ixs-popap-close');
+
+burgerButton.addEventListener('click', function(){
+  wrapperPopapBurger.classList.add('wrapper-popap-burger-open')
+});
+wrapperBurgerClose.addEventListener('click', function(){
+  wrapperPopapBurger.classList.remove('wrapper-popap-burger-open')
+});
+ixsPopapClose.addEventListener('click', function(){
+  wrapperPopapBurger.classList.remove('wrapper-popap-burger-open')
+});
+  //-----------------------carousel
+
+  const prevArrow = document.querySelector('.pets-arrow-left');
+  const nextArrow = document.querySelector('.pets-arrow-right');
+  let petsWrapperCard = document.getElementById('wrapperPets')
+  let arrCards = document.querySelectorAll('.pets-card');
+  arrCards = Array.from(arrCards);
+  
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+   nextArrow.addEventListener('click', function(){
+    shuffleArray(arrCards)
+    nextArrow.classList.add('pets-arrow-none')
+    let last = () => {
+      for(let i = 0 ; i < 6 ; i++){
+        arrCards.push(arrCards[0])
+        arrCards.shift(arrCards[0])
+        petsWrapperCard.prepend(arrCards[0])
+      }
+      nextArrow.classList.remove('pets-arrow-none')
+    }
+    setTimeout(last, 1000)
+ })
+
+ prevArrow.addEventListener('click', function(){
+  shuffleArray(arrCards)
+  prevArrow.classList.add('pets-arrow-none')
+  let last = () => {
+    for(let i = 0 ; i < 6 ; i++){
+      arrCards.push(arrCards[0])
+      arrCards.shift(arrCards[0])
+      petsWrapperCard.prepend(arrCards[0])
+    }
+    prevArrow.classList.remove('pets-arrow-none')
+  }
+  setTimeout(last, 500)
+})
+
+//----------------------------
+
+const elem = document.querySelector('input[type="range"]');
+let testimoniasWrapper = document.querySelector('.testimonias-wrapper')
+let testimCards = document.querySelectorAll('.testimonials-card');
+testimCards = Array.from(testimCards);
+
+const rangeValue = function(){
+
+  let newValue = elem.value;
+  let target = document.querySelector('.value');
+
+  if(newValue == 0){
+    testimoniasWrapper.style.left =  0 + 'px';
+    } else if ( newValue == 1){
+      testimoniasWrapper.style.left =  -306 + 'px';
+    } else if ( newValue == 2){
+      testimoniasWrapper.style.left =  -612 + 'px';
+    } else if ( newValue == 3){
+      testimoniasWrapper.style.left =  -918 + 'px';
+    } else if ( newValue == 4){
+      testimoniasWrapper.style.left =  -1224 + 'px';
+    } else if ( newValue == 5){
+      testimoniasWrapper.style.left =  -1530 + 'px';
+    } else if ( newValue == 6){
+      testimoniasWrapper.style.left =  -1836 + 'px';
+    } else if ( newValue == 7){
+      testimoniasWrapper.style.left =  -2142 + 'px';
+    } else if ( newValue == 8){
+      testimoniasWrapper.style.left =  -2448 + 'px';
+    }
+  target.innerHTML = newValue;
+}
+
+elem.addEventListener("input", rangeValue);
+
+//---------------------------------
+
+let testimonialsCard = document.querySelectorAll('.testimonials-card')
+let testimonialsCardIxs = document.querySelectorAll('.testimonials-card-ixs')
+
+testimonialsCard[0].addEventListener('click', function(){
+  testimonialsCard[0].classList.add('testimonials-card-open')
+  testimonialsCardIxs[0].classList.remove('testimonials-card-ixs-close')
+})
+testimonialsCard[1].addEventListener('click', function(){
+  testimonialsCard[1].classList.add('testimonials-card-open')
+  testimonialsCardIxs[1].classList.remove('testimonials-card-ixs-close')
+})
+testimonialsCard[2].addEventListener('click', function(){
+  testimonialsCard[2].classList.add('testimonials-card-open')
+  testimonialsCardIxs[2].classList.remove('testimonials-card-ixs-close')
+})
+ 
+  document.addEventListener( 'click', (e) => {
+    if(testimonialsCard[0].classList.contains('testimonials-card-open')){
+      const withinBoundaries = e.composedPath().includes(testimonialsCard[0]);
+      const withinBoundariesIxs = e.composedPath().includes(testimonialsCardIxs[0]);
+     if ( ! withinBoundaries || withinBoundariesIxs) {
+        testimonialsCard[0].classList.remove('testimonials-card-open')
+        testimonialsCardIxs[0].classList.add('testimonials-card-ixs-close')
+     }
+    }
+    if(testimonialsCard[1].classList.contains('testimonials-card-open')){
+      const withinBoundaries = e.composedPath().includes(testimonialsCard[1]);
+      const withinBoundariesIxs = e.composedPath().includes(testimonialsCardIxs[1]);
+     if ( ! withinBoundaries || withinBoundariesIxs) {
+        testimonialsCard[1].classList.remove('testimonials-card-open')
+        testimonialsCardIxs[1].classList.add('testimonials-card-ixs-close')
+     }
+    }
+    if(testimonialsCard[2].classList.contains('testimonials-card-open')){
+      const withinBoundaries = e.composedPath().includes(testimonialsCard[2]);
+      const withinBoundariesIxs = e.composedPath().includes(testimonialsCardIxs[2]);
+     if ( ! withinBoundaries || withinBoundariesIxs) {
+        testimonialsCard[2].classList.remove('testimonials-card-open')
+        testimonialsCardIxs[2].classList.add('testimonials-card-ixs-close')
+     }
+    }
+  })
